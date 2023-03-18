2993dfc417b96e23f5d13b74adb45be0b303c8c1
Parv Thakkar <pt21299@gmail.com>
2023-03-18 19:41:13 -0400

    Model Training and Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
+
 import os
 import sys
+
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,3 +23,43 @@ def save_object(file_path, obj):
 
     except Exception as e:
         CustomException(e, sys)
+
+
+def evaluate_models(x_train, y_train, x_test, y_test, models, params):
+
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            parameters = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, parameters, cv=3)
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file:
+            return dill.load(file)
+
+    except Exception as e:
+        raise CustomException(e, sys)
